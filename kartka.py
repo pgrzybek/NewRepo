cdf781284fd5f977e6a80e2c7e9c212973481456
PawelGrzybek <desperatos@gmail.com>
2025-08-19 14:17:31 +0200

    start

---
+++ kartka.py
@@ -0,0 +1,13 @@
+from datetime import date
+rok= date.year()
+print("Kartka z zyczeniami ")
+print("Podaj proszę imie odbiorcy")
+odbiorca=input()
+print("Podaj jego wiek")
+jegoWiek=input()
+print("Napisz swoje zyczenie")
+wiadomosc=input()
+print("Podaj swoje imie")
+imie=input()
+wiek=rok- int(jegoWiek)
+print("Wszystkiego najlepszego {imie} z okazji {wiek}  urodzin /n {wiadomosc} /n {nadawca}")
