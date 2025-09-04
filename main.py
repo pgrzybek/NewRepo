d6a416187c9492c822816f145322e6c7cca7593a
PawelGrzybek <desperatos@gmail.com>
2025-09-04 23:17:49 +0200

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from waitress import serve
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -15,19 +14,18 @@ def hello():
     return "Hello"
 
 
-# @app.route("/wynik", methods=["POST"])
-# def wynik():
-#     rokAktualny = datetime.now().year   # np. 2025
-#     imie = request.form["imie"]
-#     rok = request.form["rok"]
-#     wiadomosc = request.form["wiadomosc"]
-#     nadawca = request.form["nadawca"]
-#     wiek = rokAktualny-int(rok)
-#     # pobiera dane z formularza
-#     return f"Wszystkiego najlepszego {imie} z okazji {wiek}  urodzin <br> {wiadomosc}<br> {nadawca}"
+@app.route("/wynik", methods=["POST"])
+def wynik():
+    rokAktualny = datetime.now().year   # np. 2025
+    imie = request.form["imie"]
+    rok = request.form["rok"]
+    wiadomosc = request.form["wiadomosc"]
+    nadawca = request.form["nadawca"]
+    wiek = rokAktualny-int(rok)
+    # pobiera dane z formularza
+    return f"Wszystkiego najlepszego {imie} z okazji {wiek}  urodzin <br> {wiadomosc}<br> {nadawca}"
 
 
 if __name__ == "__main__":
-    app.run()  #to jest uruchamianie lokalnie
-
-   # serve(app, host='0.0.0.0', port=8080)# http://127.0.0.1:8080/ lokalny port uruchomiony
+    #app.run()  #to jest uruchamianie lokalnie
+    serve(app, host='0.0.0.0', port=8080)# http://127.0.0.1:8080/ lokalny port uruchomiony
